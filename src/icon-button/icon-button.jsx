04c8660fcca5aa9cd4708a532b45f3400e5548e4
Mitya Teryaew <mitya.teryaew@gmail.com>
2017-09-28 13:47:27 +0300

    feat(*): normalize closers usage among the components

--- src/icon-button/icon-button.jsx
+++ src/icon-button/icon-button.jsx
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Button from '../../button/button';
+import Button from '../button/button';
 
-import cn from '../../cn';
+import cn from '../cn';
 
 /**
  * Компонент кнопки с иконкой.
